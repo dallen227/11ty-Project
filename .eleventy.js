b08e8638364d80fb367f25f4346a33fde6ba6cc5
dallen227 <157423363+dallen227@users.noreply.github.com>
2025-03-06 16:40:40 +0000

    New Again

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,16 @@
 
-// The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
-};
-eleventyConfig.addShortcode("warning", function(text) {
-  return `<div class='warning'>⚠️ ${text}</div>`;
-});
 
-return {
-  dir: {
-      input: "src",  
-      output: "_site" 
-  }
+  eleventyConfig.addShortcode("warning", function(text) {
+      return `<div class='warning'>⚠️ ${text}</div>`;
+  });
+
+  return {
+      dir: {
+          input: ".", 
+          output: "_site"
+      }
+  };
 };
